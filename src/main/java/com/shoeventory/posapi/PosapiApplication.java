1dca3435a1f0bf65dcb93a4a5d3ff558d3818d30
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-11 14:22:24 -0400

    Initial commit

---
+++ src/main/java/com/shoeventory/posapi/PosapiApplication.java
@@ -0,0 +1,13 @@
+package com.shoeventory.posapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoeventory/posapi/PosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoeventory.posapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
