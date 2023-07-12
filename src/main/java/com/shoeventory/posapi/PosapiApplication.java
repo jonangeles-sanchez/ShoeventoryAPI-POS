7d232d96312249f951c41cf990ea239063842183
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-12 12:48:52 -0400

    Implement aws connection test

--- src/main/java/com/shoeventory/posapi/PosapiApplication.java
+++ src/main/java/com/shoeventory/posapi/PosapiApplication.java
@@ -1,13 +1,35 @@
 package com.shoeventory.posapi;
 
+import com.shoeventory.posapi.models.Shoe;
+import com.shoeventory.posapi.models.Transaction;
+import java.util.Set;
+import java.util.HashSet;
+
+import com.shoeventory.posapi.repositories.TransactionRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PosapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PosapiApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(TransactionRepository transactionRepository) {
+		// Create a fake transaction
+		Transaction transaction = new Transaction(1, 1234567890, null);
+		Set<Shoe> shoes = new HashSet<>();
+		shoes.add(new Shoe("Nike", "Air Force 1", "07", "White", "10", 1, 100, transaction));
+		shoes.add(new Shoe("Nike", "Dunks", "Panda", "White", "10", 1, 100, transaction));
+		transaction.setShoes(shoes);
+
+		return args -> {
+			transactionRepository.save(transaction);
+		};
 	}
 
 }
