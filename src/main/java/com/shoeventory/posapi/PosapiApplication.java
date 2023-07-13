9829d8283dac561026acca84dc308b117c50a723
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-13 11:52:14 -0400

    Create basic main

--- src/main/java/com/shoeventory/posapi/PosapiApplication.java
+++ src/main/java/com/shoeventory/posapi/PosapiApplication.java
@@ -5,31 +5,18 @@ import com.shoeventory.posapi.models.Transaction;
 import java.util.Set;
 import java.util.HashSet;
 
-import com.shoeventory.posapi.repositories.TransactionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.shoeventory.posapi.DAOs")
 public class PosapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PosapiApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner runner(TransactionRepository transactionRepository) {
-		// Create a fake transaction
-		Transaction transaction = new Transaction(1, 1234567890, null);
-		Set<Shoe> shoes = new HashSet<>();
-		shoes.add(new Shoe("Nike", "Air Force 1", "07", "White", "10", 1, 100, transaction));
-		shoes.add(new Shoe("Nike", "Dunks", "Panda", "White", "10", 1, 100, transaction));
-		transaction.setShoes(shoes);
-
-		return args -> {
-			transactionRepository.save(transaction);
-		};
 	}
 
 }
