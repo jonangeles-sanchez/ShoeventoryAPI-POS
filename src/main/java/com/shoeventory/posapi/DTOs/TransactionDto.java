b18f0903cbb7e372ee6324df0a844b3e86df1028
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-14 12:51:35 -0400

    Implement toString

--- src/main/java/com/shoeventory/posapi/DTOs/TransactionDto.java
+++ src/main/java/com/shoeventory/posapi/DTOs/TransactionDto.java
@@ -1,10 +1,26 @@
 package com.shoeventory.posapi.DTOs;
 
+import com.shoeventory.posapi.domain.Shoe;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record TransactionDto(
         Long id,
         Long merchantId,
-        String transactionTime
+        String transactionTime,
+        List<Shoe> shoes
 ) {
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("TransactionDto[id=").append(id())
+                .append(", merchantId=").append(merchantId())
+                .append(", transactionTime=").append(transactionTime());
+        if (shoes() != null) {
+            sb.append(", shoes=").append(shoes());
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
