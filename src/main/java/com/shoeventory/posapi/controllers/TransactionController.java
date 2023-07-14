46f95e2859ae62f1a23c4f087a0038fb694adf2f
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-14 12:22:27 -0400

    Create RestController w/createTransaction mapping

---
+++ src/main/java/com/shoeventory/posapi/controllers/TransactionController.java
@@ -0,0 +1,30 @@
+package com.shoeventory.posapi.controllers;
+
+import com.shoeventory.posapi.DTOs.TransactionDto;
+import com.shoeventory.posapi.domain.Transaction;
+import com.shoeventory.posapi.services.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/transaction")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> createTransaction(
+            @RequestBody TransactionDto req) {
+        transactionService.createTransaction(req);
+        System.out.println("In TransactionController");
+        return ResponseEntity.ok().build();
+    }
+
+}
