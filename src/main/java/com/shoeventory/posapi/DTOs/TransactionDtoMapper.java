bfec5271ca95983f2cad106fbaa34f1c316eba10
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-14 17:13:34 -0400

    Create ShoeDto List

--- src/main/java/com/shoeventory/posapi/DTOs/TransactionDtoMapper.java
+++ src/main/java/com/shoeventory/posapi/DTOs/TransactionDtoMapper.java
@@ -3,18 +3,31 @@ package com.shoeventory.posapi.DTOs;
 import com.shoeventory.posapi.domain.Transaction;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class TransactionDtoMapper implements Function<Transaction, TransactionDto> {
 
+    private final ShoeDtoMapper shoeDtoMapper;
+
+    public TransactionDtoMapper(ShoeDtoMapper shoeDtoMapper) {
+        this.shoeDtoMapper = shoeDtoMapper;
+    }
+
     @Override
     public TransactionDto apply(Transaction transaction) {
+        List<ShoeDto> shoeDtos = transaction.getShoes()
+                .stream()
+                .map(shoeDtoMapper::mapToDto)
+                .collect(Collectors.toList());
+
         return new TransactionDto(
                 transaction.getId(),
                 transaction.getMerchantId(),
                 transaction.getTransactionTime(),
-                transaction.getShoes()
+                shoeDtos
         );
     }
 }
