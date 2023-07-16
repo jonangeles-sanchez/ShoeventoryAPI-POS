4932d037cceaabc1386433a2a2bb8e1910d781ea
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-16 13:28:23 -0400

    Define updateTransaction method

--- src/main/java/com/shoeventory/posapi/DAOs/TransactionJPADataAccessService.java
+++ src/main/java/com/shoeventory/posapi/DAOs/TransactionJPADataAccessService.java
@@ -16,4 +16,31 @@ public class TransactionJPADataAccessService implements TransactionDao{
     public void createTransaction(Transaction transaction) {
         transactionRepository.save(transaction);
     }
+
+    @Override
+    public void updateTransaction(Transaction transaction) {
+        Long id = transaction.getId();
+
+        if(id == null) {
+            throw new IllegalStateException("Transaction id is null");
+        }
+
+        Transaction transactionToUpdate = transactionRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("Transaction with id " + id + " does not exist"));
+
+        if(transaction.getMerchantId() != null) {
+            transactionToUpdate.setMerchantId(transaction.getMerchantId());
+        }
+
+        if(transaction.getTransactionTime() != null){
+            transactionToUpdate.setTransactionTime(transaction.getTransactionTime());
+        }
+
+        if(transaction.getShoes() != null) {
+            transactionToUpdate.setShoes(transaction.getShoes());
+        }
+
+        transactionRepository.save(transactionToUpdate);
+
+    }
 }
