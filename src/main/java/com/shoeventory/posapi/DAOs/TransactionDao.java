23d828ae00d8c3eb4c7ec0d30149cb95d995ce96
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-16 18:07:35 -0400

    DB delete record function

--- src/main/java/com/shoeventory/posapi/DAOs/TransactionDao.java
+++ src/main/java/com/shoeventory/posapi/DAOs/TransactionDao.java
@@ -5,4 +5,5 @@ import com.shoeventory.posapi.domain.Transaction;
 public interface TransactionDao {
     void createTransaction(Transaction transaction);
     void updateTransaction(Transaction transaction);
+    void deleteTransaction(Long id);
 }
